d623ea37da34365f70078c971ccf09393042a862
Раиль <113127499+Le9o1as@users.noreply.github.com>
2023-05-29 23:12:02 +0300

    add: site

---
+++ JS/app.js
@@ -0,0 +1,5 @@
+// Нужно обратиться к 1 слову в .header__title и вставить вместо него img, или стилизовать
+
+// const headerTextElement = document.querySelector('.header__title');
+// const headerTextElementContent = headerTextElement.innerHTML;
+// headerTextElement.innerHTML = `<img src="img/Unleash.svg" alt="firstWord"> Unleash your <br> Inner Athlete`;
